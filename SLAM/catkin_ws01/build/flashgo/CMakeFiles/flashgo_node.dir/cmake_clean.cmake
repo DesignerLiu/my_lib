06749e91e5a17764c8f256cfc403b6faf303e6db
LIU <theeagleofsolomon@163.com>
2018-11-25 21:27:51 +0800

    jiguanleida

---
+++ SLAM/catkin_ws01/build/flashgo/CMakeFiles/flashgo_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flashgo_node.dir/src/flashgo_node.cpp.o"
+  "CMakeFiles/flashgo_node.dir/src/flashgo.cpp.o"
+  "/home/liu/SLAM/catkin_ws01/devel/lib/flashgo/flashgo_node.pdb"
+  "/home/liu/SLAM/catkin_ws01/devel/lib/flashgo/flashgo_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flashgo_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
